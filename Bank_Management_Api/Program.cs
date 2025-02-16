5fcac5e49b7c649f132afe0574afbb3080fffc7d
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2025-02-16 22:49:58 +0600

    Setup swaggerui # installed package Swashbuckle.AspNetCore.SwaggerUI and edit program.cs

--- Bank_Management_Api/Program.cs
+++ Bank_Management_Api/Program.cs
@@ -12,6 +12,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Bank_Management_Api");
+    });
 }
 
 app.UseHttpsRedirection();
